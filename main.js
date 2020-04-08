d1d8738dda1ebcee8ac42dd38ef5982d62bb7475
florine <florinepltg64@gmail>
2020-04-08 17:58:46 +0200

    update

--- main.js
+++ main.js
@@ -6,32 +6,44 @@
     htmlPinger = "<li class=\"_hw4\" id='0604htmlpin'><a class=\"_hw5\" href=\"#\">Pin message</a></li>";
 
     // function gestPinMessages alterate html to add the localstorage content
-    function getPinMessages(){
-        let currentPinMessages = JSON.parse(localStorage.getItem(getUrl()));
-        htmlLi = "<div id='pinDetails' class=\"1li\"><span class=\"_mh6\">";
-        if(currentPinMessages) {
-            for (let i= 0; i< currentPinMessages.length;i++){
-                htmlLi += "<div class=\"_3szo _6y4w\">" +
-                    "<div class=\"_3szp\"><img id='copy_"+ i +"' class='copy' src="+chrome.runtime.getURL("copy.svg")+" width='20px' style='margin-right: 6px' />   " +
-                    "<img id='trash_"+ i +"' class='trash' src="+chrome.runtime.getURL("trash.svg")+" width='20px'/> </div> " +
-                    "<div class=\"_3szq\" data-tooltip-content=\""+currentPinMessages[i]+"\" data-hover=\"tooltip\" data-tooltip-position=\"above\" data-tooltip-alignh=\"center\">  " + currentPinMessages[i].substring(0, 35) + "... </div>" +
-                    "</div>"
+    async function getPinMessages(){
+        return new Promise((resolve, reject)=>{
+            try{
+                chrome.storage.local.get(getUrl(), function(item) {
+                    console.log(item[getUrl()], item[getUrl()].length);
+                    htmlLi = "<div id='pinDetails' class=\"1li\"><span class=\"_mh6\">";
+                    if(item[getUrl()].length) {
+                        for (let i= 0; i< item[getUrl()].length;i++){
+                            htmlLi += "<div class=\"_3szo _6y4w\">" +
+                                "<div class=\"_3szp\"><img id='copy_"+ i +"' class='copy' src="+chrome.runtime.getURL("copy.svg")+" width='20px' style='margin-right: 6px' />   " +
+                                "<img id='trash_"+ i +"' class='trash' src="+chrome.runtime.getURL("trash.svg")+" width='20px'/> </div> " +
+                                "<div class=\"_3szq\" data-tooltip-content=\""+item[getUrl()][i].toString()+"\" data-hover=\"tooltip\" data-tooltip-position=\"above\" data-tooltip-alignh=\"center\">  " + item[getUrl()][i].toString().substring(0, 35) + "... </div>" +
+                                "</div>"
+                        }
+                        htmlLi += "</div></span>";
+                        resolve(htmlLi);
+                    }
+                    else {
+                        resolve("");
+                    }
+                });
             }
-            htmlLi += "</div></span>";
-            return htmlLi;
-        }
-        else {
-            return "";
-        }
+            catch (e) {
+                console.log("erreur")
+            }
+        })
+
     }
+
+
 
     // pinShow if "Pin Message" is clicked
     var pinShown = false;
     var previousUrl = $(location).attr('href').split('t/');
+
     function getUrl(){
         let myUrl = $(location).attr('href').split('t/');
         let url = myUrl[1];
-        console.log(url);
         if(previousUrl !== url){
             pinShown=false;
             previousUrl=url;
@@ -49,6 +61,22 @@
         }
     }
     addViewer();
+
+    function in_array(string, array){
+        var result = false;
+        for(i=0; i<array.length; i++){
+            if(array[i] === string){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    // function to refresh pin
+    function refrechPin (){
+        $("#pinDetails").remove();
+    }
+
 
 
     // when conversation is changed
@@ -70,31 +98,37 @@
     });
 
     // Add message to local storage array
-
     $(document).on("click","#0604htmlpin",function(e){
-        if (!(localStorage.getItem(getUrl()) !== null && localStorage.getItem(getUrl()).length)){
-            // local storage creation
-            var pins = [];
-            console.log('je passe par là');
-            localStorage.setItem(getUrl(), JSON.stringify(pins));
-        }
-        let currentPinMessages = JSON.parse(localStorage.getItem(getUrl()));
-        console.log(currentPinMessages);
-        currentPinMessages.push(content_message);
-        console.log(currentPinMessages);
-        localStorage.setItem(getUrl(), JSON.stringify(currentPinMessages));
-        refrechPin();
+        chrome.storage.local.get(null, function(items) {
+            let allKeys = Object.keys(items);
+            console.log(allKeys);
+            if(!in_array(getUrl(),allKeys)){
+                // local storage creation
+                var pins = [];
+                chrome.storage.local.set({[getUrl()]: pins});
+                chrome.storage.local.get(getUrl(), function (result) {
+                    console.log(getUrl());
+                    console.log(result)
+                });
+            }else {
+                chrome.storage.local.get(getUrl(), function (result) {
+                    chrome.storage.local.set({
+                        [getUrl()]: result[getUrl()] && Array.isArray(result[getUrl()])
+                            ? [...result[getUrl()], content_message]
+                            : [content_message]
+                    }, function () {
+                        console.log(result)
+                    })
+                });
+                refrechPin();
+            }
+        });
     });
 
 
 
-    // function to refresh pin
-    function refrechPin (){
-        $("#pinDetails").remove();
-    }
-
     // show or hide on click on PIN MESSAGE
-    $(document).on("click","#pinTitle",function(e){
+    $(document).on("click","#pinTitle", async function(e){
         if (pinShown){
             $("#pinDetails").hide();
             pinShown = false
@@ -104,7 +138,7 @@
                 $("#pinDetails").show();
             }else{
                 console.log($("#pinDetails"));
-                let pinMessages = getPinMessages();
+                var pinMessages = await getPinMessages();
                 $("#pinMessages").append(pinMessages);
             }
             pinShown = true
@@ -119,7 +153,19 @@
         console.log(nbItem);
 
 
-        let currentPinMessages = JSON.parse(localStorage.getItem(getUrl()));
+        chrome.storage.local.get(getUrl(), function (result) {
+            chrome.storage.local.set({
+                [getUrl()]: result[getUrl()].splice(nbItem,1)
+            }, function () {
+                console.log(result)
+            })
+        });
+        refrechPin();
+
+
+
+
+        /*let currentPinMessages = JSON.parse(localStorage.getItem(getUrl()));
         console.log(currentPinMessages);
 
         currentPinMessages.splice(nbItem,1);
@@ -127,7 +173,7 @@
         console.log(currentPinMessages);
 
         localStorage.setItem(getUrl(), JSON.stringify(currentPinMessages));
-        refrechPin();
+        refrechPin();*/
 
     });
 
@@ -137,15 +183,16 @@
         let myIdSplited = myId.split('_');
         let nbItem = myIdSplited[1];
         console.log(nbItem);
-        let currentPinMessages = JSON.parse(localStorage.getItem(getUrl()));
 
-
-        navigator.clipboard.writeText(currentPinMessages[nbItem]).then(function() {
-            $('#copy_'+nbItem).replaceWith("<img id='copy_"+ nbItem +"' class='copy' src="+chrome.runtime.getURL("tick.svg")+" width='20px' style='margin-right: 6px' />");
-            setTimeout(()=>{ $('#copy_'+nbItem).replaceWith("<img id='copy_"+ nbItem +"' class='copy' src="+chrome.runtime.getURL("copy.svg")+" width='20px' style='margin-right: 6px' />");},1500)
-        }, function() {
-            alert('erreur dans la copie du message')
+        chrome.storage.local.get(getUrl(), function(item) {
+            navigator.clipboard.writeText(item[getUrl()][nbItem]).then(function() {
+                $('#copy_'+nbItem).replaceWith("<img id='copy_"+ nbItem +"' class='copy' src="+chrome.runtime.getURL("tick.svg")+" width='20px' style='margin-right: 6px' />");
+                setTimeout(()=>{ $('#copy_'+nbItem).replaceWith("<img id='copy_"+ nbItem +"' class='copy' src="+chrome.runtime.getURL("copy.svg")+" width='20px' style='margin-right: 6px' />");},1500)
+            }, function() {
+                alert('erreur dans la copie du message')
+            });
         });
+
     });
 
 
